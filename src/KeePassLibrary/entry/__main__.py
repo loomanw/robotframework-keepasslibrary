b82932168c06cde50b0cd6849aae8e35a20a097c
William Looman <wlooman@gmail.com>
2025-01-20 08:56:59 +0100

    Cleanup

--- src/KeePassLibrary/entry/__main__.py
+++ src/KeePassLibrary/entry/__main__.py
@@ -1,10 +1,12 @@
-# Copyright 2020-     Robot Framework Foundation
+# Copyright 2008-2011 Nokia Networks
+# Copyright 2011-2016 Ryan Tomac, Ed Manlove and contributors
+# Copyright 2016-     Robot Framework Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,109 +15,31 @@
 # limitations under the License.
 
 import json
-import logging
-import os
-import re
-import subprocess
-import sys
 from pathlib import Path
-from .translation import compare_translation, get_library_translation
+from typing import Optional
 import click
 
-ROOT_FOLDER = Path(__file__).resolve().parent.parent
-is_terminal = sys.stdout.isatty()
+from .get_versions import get_version
+from .translation import compare_translation, get_library_translation
+
+
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
-# ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])", flags=re.IGNORECASE)
-logger = logging.getLogger(__name__)
+VERSION = get_version()
 
 
-def _log(message: str, silent_mode: bool = False):
-    if silent_mode:
-        return
-    if os.name == "nt":
-        message = re.sub(r"[^\x00-\x7f]", r" ", message)
-    try:
-        logger.info(message.strip("\n"))
-        if is_terminal:
-            print(message.strip("\n"), flush=True)  # noqa: T201
-    except Exception as error:
-        logger.info(f"Could not log line, suppress error {error}")
-
-
-def _write_marker(silent_mode: bool = False):
-    _log(f'\n{"=" * 70}', silent_mode)
-
-
-def _python_info():
-    _write_marker()
-    python_version = (
-        f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-    )
-    _log(f"Used Python is: {sys.executable}\nVersion: {python_version}")
-    _write_marker()
-    _log("pip freeze output:\n\n")
-    process = subprocess.run(  # noqa: PLW1510
-        [sys.executable, "-m", "pip", "freeze"],
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-        timeout=60,
-    )
-    _log(process.stdout.decode("UTF-8"))
-    _write_marker()
-
-
-def get_rf_version():
-    process = subprocess.run(
-        [sys.executable, "-m", "robot", "--version"], capture_output=True, check=False
-    )
-    return process.stdout.decode("utf-8").split(" ")[2]
-
-
-def print_version(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    _write_marker()
-    version_file = ROOT_FOLDER / "version.py"
-    version_text = version_file.read_text()
-    match = re.search(r"\"\d+\.\d+.\d+\"", version_text)
-    keepass_lib_version = match.group(0) if match else "unknown"
-    _log(f"Installed KeePassLibrary version is: {keepass_lib_version}")
-    _log(f'Robot Framework version: "{get_rf_version()}"')
-    _write_marker()
-
-
-@click.group(
-    invoke_without_command=True, context_settings=CONTEXT_SETTINGS, no_args_is_help=True
-)
-@click.option(
-    "--version",
-    is_flag=True,
-    help="Prints versions and exits",
-    callback=print_version,
-    expose_value=False,
-    is_eager=True,
-)
-@click.option(
-    "--silent", is_flag=True, help="Does not log anything, not even in the log file."
-)
-@click.pass_context
-def cli(ctx, silent):
+@click.group()
+@click.version_option(VERSION)
+def cli():
     """Robot Framework KeePassLibrary command line tool.
 
-    \b
     Possible commands are:
+
     translation
-
-    \b
-    1) pip install robotframework-browser
-    2) rfbrowser translation
-
-    translation will generate default translation json file from library keywords.
+       will generate template translation json file from library keywords.
 
     See each command argument help for more details what (optional) arguments that command supports.
     """
-    ctx.ensure_object(dict)
-    ctx.obj["SILENT"] = silent
+    pass
 
 
 @cli.command()
@@ -133,37 +57,38 @@ def cli(ctx, silent):
 )
 def translation(
     filename: Path,
+    plugins: Optional[str] = None,
     compare: bool = False,
 ):
-    """Default translation file for KeePasslibrary keywords.
+    """Default translation file from library keywords.
 
     This will help users to create their own translation as Python plugins. Command
-    will populate json file with english language. To create proper translation
+    will populate json file with English language. To create proper translation
     file, users needs to translate the keyword name and doc arguments values in
     json file.
 
     The filename argument will tell where the default json file is saved.
 
     If the --compare flag is set, then command does not generate template
-    translation file. Then it compares sha256 sums from the filenane
-    to ones read from the library documenentation. It will print out a list
-    of keywords which documentation sha256 does not match. This will ease
-    translation projects to identify keywords which documentation needs updating.
+    translation file. Instead it compares sha256 sums from the filename
+    to the one read from the library documentation. It will print out a list
+    of keywords for which the documentation sha256 does not match. This will ease
+    translating projects to identify keywords which needs updating.
     """
-    translation = get_library_translation()
+    lib_translation = get_library_translation()
     if compare:
-        if table := compare_translation(filename, translation):
-            _log(
+        if table := compare_translation(filename, lib_translation):
+            print(
                 "Found differences between translation and library, see below for details."
             )
             for line in table:
-                _log(line)
+                print(line)
         else:
-            _log("Translation is valid, no updated needed.")
+            print("Translation is valid, no updated needed.")
     else:
         with filename.open("w") as file:
-            json.dump(translation, file, indent=4)
-        _log(f"Translation file created in {filename.absolute()}")
+            json.dump(lib_translation, file, indent=4)
+        print(f"Translation file created in {filename.absolute()}")
 
 
 if __name__ == "__main__":
